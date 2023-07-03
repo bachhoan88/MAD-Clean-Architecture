1fda0043cb82e02102756534351ddb266fbf869a
Bach Ngoc Hoai <bach.ngoc.hoai@sun-asterisk.com>
2023-07-03 17:26:41 +0700

    Fix lint

--- app/src/main/java/com/example/weather/presentation/base/ExceptionHandleView.kt
+++ app/src/main/java/com/example/weather/presentation/base/ExceptionHandleView.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
